2813a8ce7f3d5da7eec7b06381bfd5c841743eb1
Yuliia Karas <iuliiakaras@gmail.com>
2023-04-10 20:50:45 +0200

    task 3 start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+const formMarkap = document.querySelector('.form');
+
+formMarkap.addEventListener('submit',(event) => event.preventDefault());
+
+const firstDelay = document.querySelector('input[name="delay"]');
+const delayStep = document.querySelector('input[name="step"]');
+const amountPromise = document.querySelector('input[name="amount"]');
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +15,12 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
